e27ffc3bab48e0a696a57838474bc5bbd931e412
VidyaGameMaka <fuzor_silverbolt@hotmail.com>
2020-05-30 13:37:58 -0400

    Created MainMenu Scene, Selected Logo Fonts and Button Fonts. Wired up main menu and setup final build settings

---
+++ Assets/_Game/Scripts/MenuSystem/MainMenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Team2Game1 {
+    public class MainMenuController : MonoBehaviour {
+        
+        public void StartGame_BTN(string sceneName) {
+            SceneManager.LoadScene(sceneName);
+        }
+
+        public void QuitGame_BTN() {
+            Application.Quit();
+        }
+
+    }
+}
