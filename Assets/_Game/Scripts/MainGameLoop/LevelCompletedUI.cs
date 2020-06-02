82f47ef9b119e718384c1421c2e262f2a8e9f6d0
VidyaGameMaka <fuzor_silverbolt@hotmail.com>
2020-06-02 19:05:45 -0400

    added bullshit

--- Assets/_Game/Scripts/MainGameLoop/LevelCompletedUI.cs
+++ Assets/_Game/Scripts/MainGameLoop/LevelCompletedUI.cs
@@ -9,10 +9,23 @@ public class LevelCompletedUI : MonoBehaviour {
 
     public TextMeshProUGUI tmp;
 
+
+    private List<string> messages = new List<string> {
+        "Brilliant job!",
+        "Outstanding work!",
+        "This is truly above and beyond.",
+        "We are thrilled to have you on our team and this is exactly why we need you.",
+        "This is superb! I had no idea a document could look this good.",
+        "To be honest, when we started the project I wasn’t sure we could pull this off but you certainly did it and did it well.",
+        "We are so fortunate to have an innovator like you on our team.",
+        "This is so great I think others could benefit from learning about it. Can I share your work at our team meeting/with my peers/with my boss, etc.?",
+        "You set a high bar with this one.",
+        "This showcases you are a role model and leader in our organization."
+    };
+
     private void OnEnable() {
-
-        tmp.text = "LEVEL Completed \n Money Earned: " + GameController.Instance.score.ToString("c2") + "\n Good Jorb!";
-
+        tmp.text = "LEVEL Completed \n Money Earned: " + GameController.Instance.score.ToString("c2") + "\n ";
+        tmp.text += messages[Random.Range(0, messages.Count)];
     }
 
 
